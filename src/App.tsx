e9a9568ac7c65889d8ec1405fb8ac218008522ed
topcheto356 <miro.kurtev27@gmail.com>
2022-08-29 22:23:24 +0300

    add vehicleController with get vehivle
    getVehicles get keyword to searrch for and options (optional) to filter the query

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React, { Fragment } from 'react';
 
 import SearchBar from './components/SearchBar';
 import Vehicle from './components/Vehicle';
+import { getVehicles } from './controllers/vehicleController';
 
 const App: React.FC = () => {
 	const onSearchHandler = (keyword: string) => {
-		console.log(keyword);
+		const options = { limit: 8, offset: 8 };
+		getVehicles(keyword, options);
 	};
 
 	return (
---
+++ src/controllers/vehicleController.tsx
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+interface options {
+	limit?: number;
+	offset?: number;
+}
+
+export const getVehicles = async (keyword: string, options?: options) => {
+	try {
+		let link: string = `https://search.outdoorsy.com/rentals?filter[keywords]=${keyword}`;
+
+		if (options) {
+			for (const option in options) {
+				link += `&page[${option}]=${options[option as keyof options]}`;
+			}
+		}
+
+		const res = await axios.get(link);
+
+		console.log(res.data.data);
+	} catch (err) {
+		console.log(err);
+	}
+};
