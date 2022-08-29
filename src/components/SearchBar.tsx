20ffbe1f770f75425250e2b93c1718825c41c092
topcheto356 <miro.kurtev27@gmail.com>
2022-08-29 22:07:48 +0300

    add useState for seached vehicle and onClickhandler

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SearchBar: React.FC = () => {
+interface SearchBarProps {
+	onSearchHandler: (searchedVehicle: string) => void;
+}
+
+interface onChangeHandlerEvent {
+	target: { value: string };
+}
+
+const SearchBar: React.FC<SearchBarProps> = (props) => {
+	const [searchedVehicle, setSearchedVehicle] = useState('');
+
+	const onChangeHandler = (event: onChangeHandlerEvent) => {
+		const keyword = event.target.value;
+		setSearchedVehicle(keyword);
+	};
+
 	return (
 		<div>
-			<input type='text' placeholder='Seach...'></input>
+			<input
+				type='text'
+				placeholder='Seach...'
+				onChange={onChangeHandler}
+				value={searchedVehicle}
+			></input>
 		</div>
 	);
 };
