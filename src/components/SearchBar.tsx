7bbc22025a7e11c733d06b93fb4b1a7794de9df4
topcheto356 <miro.kurtev27@gmail.com>
2022-08-29 21:58:09 +0300

    add SearchBarComponent

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchBar: React.FC = () => {
+	return (
+		<div>
+			<input type='text' placeholder='Seach...'></input>
+		</div>
+	);
+};
+
+export default SearchBar;
