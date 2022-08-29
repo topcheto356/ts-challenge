73c548adff2417bbf24c2ef1a1c108e14825df44
topcheto356 <miro.kurtev27@gmail.com>
2022-08-29 21:59:36 +0300

    add Vehicle component

---
+++ src/components/Vehicle.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Vehicle: React.FC = () => {
+	return (
+		<div>
+			<div>
+				<img src='#' alt='vehicle'></img>
+			</div>
+			<div>vehicle name</div>
+		</div>
+	);
+};
+
+export default Vehicle;
