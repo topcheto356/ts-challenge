98b69d4d8442bc0e9b0484cf4b3c19a4ccfb17b4
topcheto356 <miro.kurtev27@gmail.com>
2022-08-29 22:56:35 +0300

    creating interface for returned object from API

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,31 @@ import SearchBar from './components/SearchBar';
 import Vehicle from './components/Vehicle';
 import { getVehicles } from './controllers/vehicleController';
 
+interface vehiclesObject {
+	data: {
+		attributes: {
+			name: string;
+		};
+		relationships: {
+			primary_image: {
+				id: string;
+			};
+		};
+	}[];
+
+	included: {
+		id: string;
+		type: string;
+		attributes: {
+			url: string;
+		};
+	}[];
+}
+
 const App: React.FC = () => {
-	const onSearchHandler = (keyword: string) => {
+	const onSearchHandler = async (keyword: string) => {
 		const options = { limit: 8, offset: 8 };
-		getVehicles(keyword, options);
+		const allVehiclesData: vehiclesObject = await getVehicles(keyword, options);
 	};
 
 	return (
